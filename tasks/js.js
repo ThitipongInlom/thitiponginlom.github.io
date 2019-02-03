b078b71775855e501557ebf5d8184c6cb692a3d3
ThitipongInlom <ingnice007@gmail.com>
2019-02-03 19:07:04 +0700

    - Fix Jquery To Vendor

--- tasks/js.js
+++ tasks/js.js
@@ -8,7 +8,6 @@ var gulp = require('gulp'),
 module.exports = function () {
     var stream = gulp.src([
             './src/js/**/*.js', // Js Resume
-            'node_modules/jquery/dist/jquery.js',  // Jquery
             'node_modules/@fortawesome/fontawesome-free/js/all.js', // @fortawesome
             'node_modules/bootstrap/dist/js/bootstrap.js', // @Bootstrap
         ])
