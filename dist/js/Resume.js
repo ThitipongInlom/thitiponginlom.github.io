c68dc4df0464176b46196f9e91f215d1b48cbb42
ThitipongInlom <ingnice007@gmail.com>
2019-02-05 09:56:39 +0700

    - Fix animate Fadein Windows

--- dist/js/Resume.js
+++ dist/js/Resume.js
@@ -1,4 +1,17 @@
-;(function (window, $, undefined) { (function () {
+;(function (window, $, undefined) { $("#left_menu a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+        window.location.hash = hash;
+        });
+    } 
+});
+(function () {
   'use strict';
 
   var _WINDOW = {};
---
+++ src/js/animate_fadein.js
@@ -0,0 +1,14 @@
+// Menu FadeIn animate
+$("#left_menu a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+        window.location.hash = hash;
+        });
+    } 
+});
